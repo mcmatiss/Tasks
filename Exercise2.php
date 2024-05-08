4b861efe85cdc4b9d2729024d7960ee99d50bb76
mcmatiss <160856820+mcmatiss@users.noreply.github.com>
2024-05-08 20:08:18 +0300

    Delete Exercise2.php

--- Exercise2.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Create a function that accepts 2 integer arguments. First argument is a base value and the second one
-//is a value that it has been multiplied by. For example, given argument 10 and 5 prints out 50
-function multiplication ($argumentOne, $argumentTwo): int {
-    return $argumentOne * $argumentTwo;
-}
-$userIntegerInputA = (int) readline("Enter the first integer: ");
-$userIntegerInputB = (int) readline("Enter the second integer: ");
-echo multiplication($userIntegerInputA, $userIntegerInputB);
